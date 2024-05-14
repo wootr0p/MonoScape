4f31212fbda2c35f7e63de07cb26b21b22aae48a
wootrop <153186106+wootrop@users.noreply.github.com>
2024-05-14 13:55:07 +0200

    sound moving platform

--- scripts/entities/MovingPlatform.gd
+++ scripts/entities/MovingPlatform.gd
@@ -10,6 +10,9 @@ var initial_position = Vector2(0,0)
 var target_position: Vector2
 var positionA: Vector2
 var positionB: Vector2
+var prev_pos: Vector2
+var next_pos: Vector2
+var stop_sound_played : bool = true
 
 func _ready():
 	LevelManager.connect("respawn_world", self, "on_respawn_world")
@@ -26,10 +29,29 @@ func _ready():
 func reset():
 	self.position = positionA;
 	target_position = positionB;
+	stop_sound_played = true;
 	$ChangeDirectionTimer.start()
 
 func _physics_process(delta):
-	self.position = lerp(self.position, target_position, movement_speed * delta)
+	prev_pos = self.position;
+	next_pos = lerp(prev_pos, target_position, movement_speed * delta)
+	velocity = (prev_pos - next_pos)
+	
+	self.position = next_pos
+
+func _process(_delta):
+	# Gestione del suono
+	if velocity.length() > 0.5:
+		if !$Moving_Sound.playing:
+			$Moving_Sound.playing = true;
+			$Stop_Sound.playing = false;
+			stop_sound_played = false
+	else:
+		if $Moving_Sound.playing:
+			$Moving_Sound.playing = false;
+		if !$Stop_Sound.playing && !stop_sound_played:
+			$Stop_Sound.playing = true;
+			stop_sound_played = true
 
 func _on_ChangeDirectionTimer_timeout():
 	if target_position == positionA:
